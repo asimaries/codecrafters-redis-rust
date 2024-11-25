ed271a3e9bd72522bc730aa1b5f1e67522e6f7f2
asimaries <logxtech59@gmail.com>
2024-11-25 22:32:33 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,36 @@
 #![allow(unused_imports)]
-use std::{
-    io::{Read, Write},
+
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
-    thread,
 };
 
-fn handle_client(mut stream: TcpStream) {
+async fn handle_client(mut stream: TcpStream) {
     let mut buf = [0; 512];
     loop {
-        let read_count = stream.read(&mut buf).unwrap();
+        let read_count = stream.read(&mut buf).await.unwrap();
         if read_count == 0 {
             break;
         }
-        stream.write_all(b"+PONG\r\n").unwrap();
+        stream.write_all(b"+PONG\r\n").await.unwrap();
     }
-    stream.flush().unwrap()
+    stream.flush().await.unwrap();
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
     //
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-
-    for stream in listener.incoming() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    loop {
+        let stream = listener.accept().await;
         match stream {
-            Ok(stream) => {
-                thread::spawn(|| {
-                    handle_client(stream);
+            Ok((stream, _)) => {
+                tokio::spawn(async move {
+                    handle_client(stream).await;
                 });
             }
             Err(e) => {
